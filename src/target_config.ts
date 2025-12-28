5c45c67c38cb979897a022c0a7b0807a465ea7dc
nylox23 <nylox23@gmail.com>
2025-12-28 23:16:04 +0300

    Update target_config.ts

--- src/target_config.ts
+++ src/target_config.ts
@@ -4,4 +4,4 @@ export const api_proxy_addr = "http://192.168.1.65:8080";
 
 export const dest_api = (target_tauri) ? api_proxy_addr + "/api" : "/api";
 
-export const dest_root = (target_tauri) ? "" : "/acid-app";
+export const dest_root = (target_tauri) ? "" : "/RIP-SPA_React";
