8ef44aa4d8d16dc6558d83634d7730570bd0074e
nylox23 <nylox23@gmail.com>
2025-12-28 18:38:02 +0300

    Add files via upload

--- src/slices/acidsSlice.ts
+++ src/slices/acidsSlice.ts
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 import { useSelector } from "react-redux";
 import type { Acid } from "../modules/types";
-import type { RootState } from "../store"; // Мы создадим store на следующем шаге
+import type { RootState } from "../store";
 
 interface AcidsState {
     Data: Acid[];
